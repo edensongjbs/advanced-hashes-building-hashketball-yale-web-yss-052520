4201a9b625e25af1c40273fab82c9b9730edc33b
James B Schoen <edensongjbs@gmail.com>
2020-05-11 13:55:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,15 @@
 # Write your code here!
+def game_hash
+  {
+    home:{
+      team name:"",
+      colors:[],
+      players:[]
+    }, 
+    away:{
+      team name:"",
+      colors:[],
+      players:[]
+    }
+  }
+end
